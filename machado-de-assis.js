11968baeab6067e2f0ec07ea6f66a810942eade5
Bruno <bruno.umbelino0@gmail.com>
2021-06-02 10:30:06 -0300

    second commit

--- machado-de-assis.js
+++ machado-de-assis.js
@@ -1,2 +1,7 @@
-const frase1 = `Deus, para a felicidade do homem, inventou a fé e o amor. O Diabo, invejoso, 
+const frase1 = `Deus, para a felicidade do homem, 
+inventou a fé e o amor. O Diabo, invejoso, 
 fez o homem confundir fé com religião e amor com casamento.`;
+
+const frase2 = `Esquecer é uma necessidade. 
+A vida é uma lousa, em que o destino, 
+para escrever um novo caso, precisa de apagar o caso escrito.`;
