93b0bcc9d47bdcf4ec07a49bf4ead4619510f088
Bruno <bruno.umbelino0@gmail.com>
2021-06-02 11:17:25 -0300

    foruth commit altered

--- machado-de-assis.js
+++ machado-de-assis.js
@@ -5,3 +5,6 @@ fez o homem confundir fé com religião e amor com casamento.`;
 const frase2 = `Esquecer é uma necessidade. 
 A vida é uma lousa, em que o destino, 
 para escrever um novo caso, precisa de apagar o caso escrito.`;
+
+const frase3 = `O dinheiro não traz felicidade 
+— para quem não sabe o que fazer com ele.`;
