14f164239c33ec88ad2993c1c7cae760beedb99f
Bruno <bruno.umbelino0@gmail.com>
2021-06-02 14:53:42 -0300

    alterando formatação do texto

--- machado-de-assis.js
+++ machado-de-assis.js
@@ -2,8 +2,7 @@ const frase1 = `Deus, para a felicidade do homem,
 inventou a fé e o amor. O Diabo, invejoso, 
 fez o homem confundir fé com religião e amor com casamento.`;
 
-const frase2 = `Esquecer é uma necessidade. 
-A vida é uma lousa, em que o destino, 
+const frase2 = `Esquecer é uma necessidade. A vida é uma lousa, em que o destino, 
 para escrever um novo caso, precisa de apagar o caso escrito.`;
 
 const frase3 = `O dinheiro não traz felicidade 
